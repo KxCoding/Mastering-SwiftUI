3c04b18993ca8ffc19a01f249bdbf03a3f218992
kei <kky0317@gmail.com>
2022-02-21 15:47:41 +0900

    Add Controls Project (Xcode 13)

---
+++ SwiftUI3/Controls/Shared/ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlsApp.swift
+//  Shared
+//
+//  Created by KxCoding on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainList()
+        }
+    }
+}
